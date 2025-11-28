ec4b0edb531b83a06e41be95ff2ae8565d73fc55
kritiadhikari135 <kriteeadhikari90@gmail.com>
2025-11-28 15:54:01 +0545

    initialization and first program in spring with xml

---
+++ src/main/java/org/example/Dog.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Dog {
+
+    public Dog(){
+        System.out.println("This is a object from dog constructor");
+    }
+    public void speak(){
+        System.out.println("Bark");
+    }
+}
